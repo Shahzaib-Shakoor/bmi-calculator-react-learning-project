eb55b62e486bb36835ddd0c0f32dcefb3647271e
Shahzaib Shakoor <shahzaib.shakoor2199@gmail.com>
2023-10-11 14:48:19 +0500

    First commit 🚀

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title />
+      <Calculator />
     </div>
   );
 }
 
-export default App;
+function Title() {
+  return <div className="Title">BMI Calculator</div>;
+}
+
+function Calculator() {
+  const [bmi, setBmi] = useState(0);
+
+  return (
+    <div className="Calculator">
+      <InputSection setBmi={setBmi} />
+      <OutputSection bmi={bmi} />
+    </div>
+  );
+}
+
+function InputSection({ setBmi }) {
+  const [weight, setWeight] = useState(50);
+  const [height, setHeight] = useState(150);
+
+  useEffect(() => {
+    const heightMeters = height / 100;
+    const bmi = weight / (heightMeters * heightMeters);
+    setBmi(bmi.toFixed(2));
+  }, [weight, height]);
+
+  return (
+    <div className="InputSection">
+      <InputSlider min={40} max={220} value={weight} onChange={setWeight}>
+        Weight: {weight} kg
+      </InputSlider>
+
+      <InputSlider min={140} max={220} value={height} onChange={setHeight}>
+        Height: {height} cm
+      </InputSlider>
+    </div>
+  );
+}
+
+function InputSlider({ min, max, value, onChange, children }) {
+  return (
+    <div className="InputSlider">
+      <label>{children}</label>
+      <input
+        type="range"
+        min={min}
+        max={max}
+        value={value}
+        onChange={e => onChange(Number(e.target.value))}
+      />
+    </div>
+  );
+}
+
+function OutputSection({ bmi }) {
+  return (
+    <div className="OutputSection">
+      <p>Your BMI is</p>
+      <p className="output">{bmi}</p>
+    </div>
+  );
+}
